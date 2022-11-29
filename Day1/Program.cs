8b692464f2bfb25c369457001a15ccce56c1e53f
Gustav Kron <gustav@vindsrummet.com>
2022-11-29 20:57:45 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Runtime.ExceptionServices;
+
+string puzzleSearchPath = (@"C:\Users\gusta\source\repos\AdventOfCode2022\Day1\puzzleInput.txt");
+var puzzleInput = File.ReadAllLines(puzzleSearchPath).ToList();
+
+Console.WriteLine("Svar del ett: " + PartOne(puzzleInput));
+Console.WriteLine("Svar del två: " + PartTwo(puzzleInput));
+
+
+static int PartOne(List<string> input)
+{
+    return 0;
+}
+
+static int PartTwo(List<string> input)
+{
+    return 0;
+}
