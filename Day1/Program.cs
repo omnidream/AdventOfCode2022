70432684ea4a3c67bcf436fd8775b9bf2497a7c8
gustav.kron@knowit.se <gustav.kron@knowit.se>
2022-12-01 11:34:21 +0100

    Refactoring Day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -4,56 +4,48 @@ using System.Runtime.ExceptionServices;
 
 string puzzleSearchPath = (@"C:\Users\guskro\Source\Repos\AdventOfCode2022\Day1\puzzleInput.txt");
 var puzzleInput = File.ReadAllLines(puzzleSearchPath).ToList();
-List<Elf> elvs = new List<Elf>();
+List<int> elvs = new List<int>();
 
 Console.WriteLine("Svar del ett: " + PartOne(puzzleInput, elvs));
 Console.WriteLine("Svar del två: " + PartTwo(puzzleInput, elvs));
 
 
-static int PartOne(List<string> input, List<Elf> elfs)
+static int PartOne(List<string> input, List<int> elfs)
 {
 	int sum = 0;
 	foreach (var line in input)
 	{
 		if(line == String.Empty)
 		{
-			Elf myElf = new();
-			myElf.TotalCalories = sum;
-			elfs.Add(myElf);
+			elfs.Add(sum);
 			sum = 0;
 		}
 		else
 			sum = sum + Int32.Parse(line);	
 	}
-	Elf loadedElf = GetTheMostStockedElf(elfs);
-    return loadedElf.TotalCalories;
+	int loadedElf = GetTheMostStockedElf(elfs);
+    return loadedElf;
 }
 
-static int PartTwo(List<string> input, List<Elf> elfs)
+static int PartTwo(List<string> input, List<int> elfs)
 {
 	int sum = 0;
-    List<Elf> SortedElvs = elfs.OrderByDescending(o => o.TotalCalories).ToList();
+    List<int> SortedElvs = elfs.OrderByDescending(o => o).ToList();
 	for (int i = 0; i < 3; i++)
 	{
-		sum = sum + SortedElvs[i].TotalCalories;
+		sum = sum + SortedElvs[i];
 	}
     return sum;
 }
 
-static Elf GetTheMostStockedElf(List<Elf> elvs)
+static int GetTheMostStockedElf(List<int> elvs)
 {
-	Elf mostStockedElf = new();
+	int mostStockedElf = 0;
 
 	foreach (var elf in elvs)
 	{
-		if (elf.TotalCalories > mostStockedElf.TotalCalories)
+		if (elf > mostStockedElf)
 			mostStockedElf = elf;
 	}
 	return mostStockedElf;
-}
-class Elf
-{
-    public List<int> FoodList { get; set; }
-	public int TotalCalories { get; set; }
-
 }
