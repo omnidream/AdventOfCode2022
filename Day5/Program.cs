af6f3e7b91bac578bd0dbed5cf60c482720848e2
Gustav Kron <gustav@vindsrummet.com>
2022-12-06 22:17:56 +0100

    Started Day5

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,17 +1,43 @@
 ﻿
-string puzzleSearchPath = (@"C:\Users\gusta\source\repos\AdventOfCode2022\Day1\puzzleInput.txt");
-var puzzleInput = File.ReadAllLines(puzzleSearchPath).ToList();
+using System.Numerics;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Svar del ett: " + PartOne(puzzleInput));
-Console.WriteLine("Svar del två: " + PartTwo(puzzleInput));
-
-
-static int PartOne(List<string> input)
+internal class Program
 {
-    return 0;
-}
+    
 
-static int PartTwo(List<string> input)
-{
-    return 0;
+    private static void Main(string[] args)
+    {
+        string puzzleSearchPath = @"C:\Users\gusta\source\repos\AdventOfCode2022\Day5\puzzleInput.txt";
+        var puzzleInput = File.ReadAllLines(puzzleSearchPath).ToList();
+
+        //    [D]
+        //[N] [C]
+        //[Z] [M] [P]
+        // 1   2   3
+
+        //Initiate stacked creates
+        List<char> one = new() { 'N', 'Z' };
+        List<char> two = new() { 'D', 'C', 'M' };
+        List<char> three = new() { 'P' };
+
+        Console.WriteLine("Svar del ett: " + PartOne(puzzleInput));
+        Console.WriteLine("Svar del två: " + PartTwo(puzzleInput));
+
+        int PartOne(List<string> input)
+        {
+            foreach (var moveString in input)
+            {
+                int move = int.Parse(new string(moveString.Where(char.IsDigit).ToString()));
+            }
+            one.Insert(0, 'D');
+            one.RemoveAt(0);
+            return 0;
+        }
+
+        int PartTwo(List<string> input)
+        {
+            return 0;
+        }
+    }
 }
